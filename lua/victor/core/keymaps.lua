eeca0b370e1b4587bab900bf219c00aa56f57654
COLINA Victor <colinavictor@MacBook-Pro-de-Servicedesk.local>
2024-12-21 11:43:38 -0300

    feature: adding astro support

--- lua/victor/core/keymaps.lua
+++ lua/victor/core/keymaps.lua
@@ -18,8 +18,8 @@ keymap.set("n", "<leader>wa", ":wa<CR>", { desc = "Save changes in all files" })
 -- clear search highlights
 keymap.set("n", "<leader>nh", ":nohl<CR>", { desc = "Clear search highlights" })
 
--- quit nvim
-keymap.set("n", "<leader>qq", ":q<CR>", { desc = "Quit nvim" })
+-- quit all nvim
+keymap.set("n", "<leader>qq", ":qa<CR>", { desc = "Quit nvim" })
 
 -- delete single character without copying into register
 -- keymap.set("n", "x", '"_x')
@@ -36,10 +36,15 @@ keymap.set("n", "<leader>sh", "<C-w>s", { desc = "Split window horizontally" }) 
 keymap.set("n", "<leader>se", "<C-w>=", { desc = "Make splits equal size" }) -- make split windows equal width & height
 keymap.set("n", "<leader>sx", "<cmd>close<CR>", { desc = "Close current split" }) -- close current split window
 
+-- tab management
 keymap.set("n", "<leader>to", "<cmd>tabnew<CR>", { desc = "Open new tab" }) -- open new tab
 keymap.set("n", "<leader>tx", "<cmd>tabclose<CR>", { desc = "Close current tab" }) -- close current tab
 keymap.set("n", "<leader>tn", "<cmd>tabn<CR>", { desc = "Go to next tab" }) --  go to next tab
 keymap.set("n", "<leader>tp", "<cmd>tabp<CR>", { desc = "Go to previous tab" }) --  go to previous tab
 keymap.set("n", "<leader>tf", "<cmd>tabnew %<CR>", { desc = "Open current buffer in new tab" }) --  move current buffer to new tab
+
+-- tab navigation (overwrite)
+keymap.set("n", "<S-l>", "<cmd>tabn<CR>", { desc = "Go to next tab" }) -- go to next tab
+keymap.set("n", "<S-h>", "<cmd>tabp<CR>", { desc = "Go to next tab" }) -- go to previous tab
 
 keymap.set("n", "<leader>ml", "<C-v>", { desc = "Insert the same characters across multiple lines" }) -- insert the same characters across multiple lines
