d13a6f0303256785080718d7cd6840ca38ae2f4e
COLINA Victor <colinavictor@MacBook-Pro-de-Servicedesk.local>
2024-12-21 12:12:03 -0300

    fix: fixing autotag using nvim-ts-autotag

--- lua/victor/plugins/treesitter.lua
+++ lua/victor/plugins/treesitter.lua
@@ -16,10 +16,8 @@ return {
 			},
 			-- enable indentation
 			indent = { enable = true },
-			-- enable autotagging (w/ nvim-ts-autotag plugin)
-			autotag = {
-				enable = true,
-			},
+			-- auto install missing parsers
+			auto_install = true,
 			-- ensure these language parsers are installed
 			ensure_installed = {
 				"bash",
